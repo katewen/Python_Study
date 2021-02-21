986fa9ba43ae1205b66de8d19cc24434b2cf9353
Erik <1372432518@qq.com>
2021-02-21 15:52:11 +0800

    完善上传ipa脚本 完成打包xcframework

--- uploadIpa.py
+++ uploadIpa.py
@@ -1,6 +1,8 @@
 
-#自动打包上传ipa 待完善
+#自动打包上传ipa
 import os
+import subprocess
+import sys
 
 projectPath = input("请输入项目绝对路径：")
 projectName = input("请输入项目名称：")
@@ -12,9 +14,22 @@ cleanShell = "xcodebuild clean -workspace "+projectPath + " -scheme " + projectN
 archiveShell = "xcodebuild clean -workspace "+projectPath + " -scheme " + projectName + " -archivePath " + archivePath + "/" + projectName + ".xcarchive"
 # ipa 需要配置打包plist文件 选择 adhoc App store
 exportShell = "xcodebuild -exportArchive -archivePath " + archivePath + "/" + projectName + ".xcarchive" + " -exportPath " + archivePath + " -exportOptionsPlist " + configPath
-# 上传ipa
 
-#上传bugly符号表
+os.system(cleanShell)
+os.system(archiveShell)
+os.system(exportShell)
+os.system("open "+archivePath)
+# subprocess.getoutput(cleanShell)
+# subprocess.getoutput(archiveShell)
+# subprocess.getoutput(exportShell)
 
-
+isUpload = input("打包IPA完成，是否上传App Store Connect:")
+if isUpload == "yes":
+    username = input("请输入Apple Id：")
+    appPw = input("请输入App专用密码非AppID密码：")
+    # 上传ipa
+    uploadIpa = "xcrun altool --upload-app -f " + archivePath + "/" + projectName + ".ipa" + " -u " + username + " -p "+appPw
+    os.system(uploadIpa)
+else:
+    sys.exit(0)
 
